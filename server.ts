b75e60ea5368a8197571fd183b3f812b6ab6284e
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-12-10 16:26:09 +0100

    heroku updates

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
-import app from "./index";
+import app from "./dist/index";
 
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 app.listen(port, (): void => {
   console.log(`Listening on port ${port}`);
